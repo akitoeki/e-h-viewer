ca491ca68dea4d3f5d60c09a8f6ff417a46ee660
Nhan Dang <nhan@seespace.co>
2014-02-17 13:28:23 +0700

    Init files

---
+++ getImages.js
@@ -0,0 +1,106 @@
+  var url = document.URL;
+  var ss = url.split("/");
+  var images = new Array();
+  var thumbs = new Array();
+  var started = false;
+
+  var count = 0;
+
+  var thread = 0;
+  var maxThread = 3;
+
+  var threadLife = new Array();
+  var imgLoadTimeout = 20000;
+
+  var timeout = 0;
+
+  console.log(ss);
+
+
+  //Get GID, Token & Image links
+
+  if (ss[3] == "g") {
+    var total = parseInt($(".ptt td").last().prev().children().html());
+
+    var cleanUrl = "http://g.e-hentai.org/g/"+ss[4]+"/"+ss[5]+"/?p=";
+    console.log(cleanUrl);
+
+    //Proceed to get the links
+
+
+    for (var i = 0; i < total; i++) {
+      var page = cleanUrl+i;
+      setTimeout(function(s){
+        return function() {
+          // console.log(">>> parse: " + s);
+          $.get(s, function(result) {
+            $(result).find(".gdtl").each(function(){
+              // console.log($(this).children("a").attr("href"));
+              count++;
+              images.push($(this).children("a").attr("href"));
+              thumbs.push($(this).children("a").html());
+              startDownload();
+            });
+          });
+        };
+      }(page), i * 3000);
+    }
+  }
+
+  function startDownload() {
+    if (!started) {
+
+      //Clear
+      $("iframe").remove();
+      var desc = $(".gm").first();
+
+      $("body").html("");
+      $("body").append(desc);
+      $("body").css({
+        "text-align":"center",
+        "background":"#000"
+      });
+
+
+
+      console.log("Start Download");
+      started = true;
+
+
+      var loop = setInterval(function() {
+        if (thread < maxThread) {
+          if (images.length) {
+            var div = $("<div style='padding-bottom: 10px'></div>");
+            var link = images.shift();
+            div.load(link+" #img", function(){
+              $("body").append(div);
+              div.children("img").css({
+                "width":"1000px",
+                "height": "auto"});
+              div.children("img").one('load', function() {
+                // do stuff
+                if (thread > 0) {
+                  thread--;
+                }
+                console.log('load done');
+              }).each(function() {
+                if(this.complete) $(this).load();
+              });
+
+            });
+            thread++;
+          }
+        } else {
+          if (images.length > 0) {
+            timeout += 1000;
+            if (timeout > imgLoadTimeout) {
+              thread = 0;
+              timeout = 0;
+            }
+          }
+        }
+      },1000);
+
+    }
+  }
+
