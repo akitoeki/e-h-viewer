558f3391e19ee6b2cae51d74bf6a6feb534719b7
VND Dang, Nhan <Dang.Nhan@bcg.com>
2024-10-28 23:15:20 +0700

    update get images

--- getImages.js
+++ getImages.js
@@ -1,106 +1,115 @@
-  var url = document.URL;
-  var ss = url.split("/");
-  var images = new Array();
-  var thumbs = new Array();
-  var started = false;
+var url = document.URL;
+var ss = url.split("/");
+var images = [];
+var thumbs = [];
+var started = false;
 
-  var count = 0;
+var count = 0;
+var thread = 0;
+var maxThread = 3;
+var threadLife = [];
+var imgLoadTimeout = 20000;
+var timeout = 0;
 
-  var thread = 0;
-  var maxThread = 3;
+console.log(ss);
 
-  var threadLife = new Array();
-  var imgLoadTimeout = 20000;
+// Get GID, Token & Image links
 
-  var timeout = 0;
+if (ss[3] === "g") {
+    var total = parseInt(document.querySelector(".ptt td:last-child").previousElementSibling.children[0].innerHTML);
+    console.log({ total });
+    var cleanUrl = "https://e-hentai.org/g/" + ss[4] + "/" + ss[5] + "/?p=";
+    console.log({ cleanUrl });
 
-  console.log(ss);
-
-
-  //Get GID, Token & Image links
-
-
-  if (ss[3] == "g") {
-    var total = parseInt($(".ptt td").last().prev().children().html());
-    console.log(total)
-    var cleanUrl = "https://e-hentai.org/g/"+ss[4]+"/"+ss[5]+"/?p=";
-    console.log(cleanUrl);
-
-    //Proceed to get the links
+    // Proceed to get the links
     for (var i = 0; i < total; i++) {
-      var page = cleanUrl+i;
-      setTimeout(function(s){
-        return function() {
-          // console.log(">>> parse: " + s);
-          $.get(s, function(result) {
-            $(result).find(".gdtm").each(function(){
-              console.log($(this).find("a").attr('href'));
-              count++;
-              images.push($(this).find("a").attr("href"));
-              thumbs.push($(this).find("a").html());
-              startDownload();
-            });
-            console.log(images)
-          });
-        };
-      }(page), i * 3000);
-    }
-  }
+        var page = cleanUrl + i;
+        setTimeout(function (s) {
+            return function () {
+                fetch(s).then(response => response.text()).then(result => {
+                    var parser = new DOMParser();
+                    var doc = parser.parseFromString(result, 'text/html');
+                    var gt100Elements = doc.querySelectorAll(".gt100");
 
-  function startDownload() {
+                    gt100Elements.forEach(function (element) {
+
+                      var links = element.querySelectorAll("a");
+                      
+                      links.forEach(function(linkElement) {
+                          var link = linkElement.getAttribute('href');
+                          console.log(link);
+                          count++;
+                          images.push(link);
+                          thumbs.push(linkElement.innerHTML);
+                          startDownload();
+                      });
+                  });
+
+                    console.log(images);
+                });
+            };
+        }(page), i * 3000);
+    }
+}
+
+function startDownload() {
     if (!started) {
+        // Clear
+        document.querySelectorAll("iframe").forEach(iframe => iframe.remove());
+        var desc = document.querySelector(".gm");
 
-      //Clear
-      $("iframe").remove();
-      var desc = $(".gm").first();
+        document.body.innerHTML = "";
+        document.body.appendChild(desc);
+        document.body.style.textAlign = "center";
+        document.body.style.background = "#000";
 
-      $("body").html("");
-      $("body").append(desc);
-      $("body").css({
-        "text-align":"center",
-        "background":"#000"
-      });
+        console.log("Start Download");
+        started = true;
 
+        var loop = setInterval(function () {
+            if (thread < maxThread) {
+                if (images.length) {
+                    var div = document.createElement("div");
+                    div.style.paddingBottom = "10px";
 
+                    var link = images.shift();
+                    fetch(link).then(response => response.text()).then(result => {
+                        var parser = new DOMParser();
+                        var doc = parser.parseFromString(result, 'text/html');
+                        var img = doc.querySelector("#img");
 
-      console.log("Start Download");
-      started = true;
+                        if (img) {
+                            var imgElement = img.cloneNode(true);
+                            imgElement.style.width = "1000px";
+                            imgElement.style.height = "auto";
 
+                            div.appendChild(imgElement);
+                            document.body.appendChild(div);
 
-      var loop = setInterval(function() {
-        if (thread < maxThread) {
-          if (images.length) {
-            var div = $("<div style='padding-bottom: 10px'></div>");
-            var link = images.shift();
-            div.load(link+" #img", function(){
-              $("body").append(div);
-              div.children("img").css({
-                "width":"1000px",
-                "height": "auto"});
-              div.children("img").one('load', function() {
-                // do stuff
-                if (thread > 0) {
-                  thread--;
+                            imgElement.onload = function () {
+                                if (thread > 0) {
+                                    thread--;
+                                }
+                                console.log('load done');
+                            };
+
+                            if (imgElement.complete) {
+                                imgElement.onload();
+                            }
+
+                            thread++;
+                        }
+                    });
                 }
-                console.log('load done');
-              }).each(function() {
-                if(this.complete) $(this).load();
-              });
-
-            });
-            thread++;
-          }
-        } else {
-          if (images.length > 0) {
-            timeout += 1000;
-            if (timeout > imgLoadTimeout) {
-              thread = 0;
-              timeout = 0;
+            } else {
+                if (images.length > 0) {
+                    timeout += 1000;
+                    if (timeout > imgLoadTimeout) {
+                        thread = 0;
+                        timeout = 0;
+                    }
+                }
             }
-          }
-        }
-      },1000);
-
+        }, 1000);
     }
-  }
-
+}
